06fd3d80be35cb7034b36c6287eb383b55a9d964
Eko <ekovegeance7@gmail.com>
2025-06-05 17:24:07 +0700

    refactor: rename layout and page files for improved structure

--- lib/source.ts
+++ lib/source.ts
@@ -6,7 +6,7 @@ import {createElement} from "react";
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
-  baseUrl: '/docs',
+  baseUrl: '/',
   source: docs.toFumadocsSource(),
 
   icon(icon) {
