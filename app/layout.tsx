f02c90634ad9e7d5d62e473d27ab2105e5e3bf52
Eko <ekovegeance7@gmail.com>
2026-04-08 10:31:34 +0700

    docs: add initial documentation structure and setup guide for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Plus_Jakarta_Sans } from 'next/font/google';
+import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Geist } from 'next/font/google';
 import type { ReactNode } from 'react';
 import {Banner} from "fumadocs-ui/components/banner";
 
 
-const plusJakartaSans = Plus_Jakarta_Sans({
+const plusJakartaSans = Geist({
     subsets: ['latin'],
     weight: ['400', '500', '600', '700', '800'],
 });
--- lib/source.ts
+++ lib/source.ts
@@ -1,17 +1,11 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
-import { icons } from 'lucide-react';
-import {createElement} from "react";
+import {docs} from 'collections/server';
+import {loader} from 'fumadocs-core/source';
+import {lucideIconsPlugin} from "fumadocs-core/source/lucide-icons";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
-  // it assigns a URL to your pages
-  baseUrl: '/',
-  icon(icon) {
-    if (!icon) {
-      return;
-    }
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
-  },
-  source: docs.toFumadocsSource(),
+    // it assigns a URL to your pages
+    baseUrl: '/',
+    source: docs.toFumadocsSource(),
+    plugins: [lucideIconsPlugin()],
 });
